b37e886c4286913fcc186ea03e7e0c86c5d52057
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-21 18:56:19 +0100

    Navigation to GameScreen

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
-  return (
-    <View style={styles.screen}>
-      <Header title="Guess a number"/>
-      <StartGameScreen/>
-    </View>
-  );
+    const [userNumber, setUserNumber] = useState();
+
+    const startGameHandler = (selectedNumber) => {
+        setUserNumber(selectedNumber);
+    };
+
+    let content = <StartGameScreen onStartGame={startGameHandler}/>;
+
+    if(userNumber){
+        content = <GameScreen userChoice={userNumber}/>;
+    }
+
+    return (
+        <View style={styles.screen}>
+            <Header title="Guess a number"/>
+            {content}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
+    screen: {
+        flex: 1,
+    },
 });
